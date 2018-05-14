b0b4b2962200eb6395e587a9476beb0abd8be5d0
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2018-05-15 02:17:24 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -44,6 +44,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
+			//file_put_contents("php://stderr", "reply result: " . $result . "\r\n");
 			
 			$Topic = "NodeMCU1" ;
 			$text = $event['message']['text'];
