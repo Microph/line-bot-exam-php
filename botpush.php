3c0022cab84c3f54a56b55dc1223e4d30420ea40
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2018-05-15 19:18:13 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -3,24 +3,46 @@
 require "vendor/autoload.php";
 
 foreach ($_POST as $key => $value){
-   file_put_contents("php://stderr", "post text from arduino key: " . $key . " value: " . $value . "\n");
+   file_put_contents("php://stderr", "post text from arduino key: " . $key . "\nvalue: " . $value . "\n");
+   if($key == "text")
+   { 
+      send_push_message($value);
+   }
 }
+
+$access_token = 'gfFXBM/EbegiS1D3eWlCV64GBADykoNE8YxuDepBcp1+YSqUcdYv0HU8Afzr2rq+qkBrbXF3h+auDA/qRS60wKIN4pcX+bGoc5FQOUWoERdjEHMOpljuKpEh1e2VmocNghJwLR9W9C4yQIHMc8xsKwdB04t89/1O/w1cDnyilFU=';
 
 function send_push_message($msg){
+   $channelSecret = '5a4a6b9b0cf3415b8071f4ffbe08fe3d';
 
-    $access_token = 'gfFXBM/EbegiS1D3eWlCV64GBADykoNE8YxuDepBcp1+YSqUcdYv0HU8Afzr2rq+qkBrbXF3h+auDA/qRS60wKIN4pcX+bGoc5FQOUWoERdjEHMOpljuKpEh1e2VmocNghJwLR9W9C4yQIHMc8xsKwdB04t89/1O/w1cDnyilFU=';
+   $pushID = 'Ud168d219c6397f81416451f5ffc0b081';
 
-    $channelSecret = '5a4a6b9b0cf3415b8071f4ffbe08fe3d';
+   $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+   $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-    $pushID = 'Ud168d219c6397f81416451f5ffc0b081';
+   $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
+   $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
-    $response = $bot->pushMessage($pushID, $textMessageBuilder);
-
-    file_put_contents("php://stderr", "push message result: " . $response->getHTTPStatus() . ' ' . $response->getRawBody());
-
+   file_put_contents("php://stderr", "push message result: " . $response->getHTTPStatus() . ' ' . $response->getRawBody());
 }
 
+function send_reply_message($msg, $replyToken){
+   // Make a POST Request to Messaging API to reply to sender
+   $url = 'https://api.line.me/v2/bot/message/reply';
+   $data = [
+      'replyToken' => $replyToken,
+      'messages' => [$messages],
+   ];
+   $post = json_encode($data);
+   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+   $ch = curl_init($url);
+   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   $result = curl_exec($ch);
+   //file_put_contents("php://stderr", "reply result: " . $result . "\r\n");
+   curl_close($ch);
+}
