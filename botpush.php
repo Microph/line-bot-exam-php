08694b882b3a48f57d8314e80372316a3083e789
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2018-05-17 01:15:42 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,7 +4,7 @@ require "vendor/autoload.php";
 
 $messagingType = "";
 $replyToken = "";
-$pushID = "";
+$userID = "";
 foreach ($_POST as $key => $value){
    file_put_contents("php://stderr", "post text from arduino key: " . $key . "\nvalue: " . $value . "\n");
    if($key == "messagingType")
@@ -13,7 +13,7 @@ foreach ($_POST as $key => $value){
    }
    else if($key == "userID")
    {
-      $pushID = $value;
+      $userID = $value;
    }
    else if($key == "replyToken")
    {
@@ -27,14 +27,13 @@ foreach ($_POST as $key => $value){
       }
       else
       {
-         send_push_message($value, $pushID);
+         send_push_message($value, $userID);
       }
    }
 }
 
 //Can only send to one specific account -> need database implementation to store pushIDs
-function send_push_message($msg, $blah){
-   $pushID = 'ud168d219c6397f81416451f5ffc0b081';
+function send_push_message($msg, $userID){
    $access_token = 'gfFXBM/EbegiS1D3eWlCV64GBADykoNE8YxuDepBcp1+YSqUcdYv0HU8Afzr2rq+qkBrbXF3h+auDA/qRS60wKIN4pcX+bGoc5FQOUWoERdjEHMOpljuKpEh1e2VmocNghJwLR9W9C4yQIHMc8xsKwdB04t89/1O/w1cDnyilFU=';
    $channelSecret = '5a4a6b9b0cf3415b8071f4ffbe08fe3d';
 
@@ -42,7 +41,7 @@ function send_push_message($msg, $blah){
    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
-   $response = $bot->pushMessage($pushID, $textMessageBuilder);
+   $response = $bot->pushMessage($userID, $textMessageBuilder);
 
    file_put_contents("php://stderr", "push message result: " . $response->getHTTPStatus() . ' ' . $response->getRawBody());
 }
